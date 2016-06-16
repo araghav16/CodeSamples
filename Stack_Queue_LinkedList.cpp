d1df0c96db02c12bb2e0f03399ba4563ba9314d5
araghav <araghav16@users.noreply.github.com>
2016-06-16 14:59:26 +0100

    Update Stack_Queue_LinkedList.cpp

--- Stack_Queue_LinkedList.cpp
+++ Stack_Queue_LinkedList.cpp
@@ -7,28 +7,28 @@ using namespace std;
 ** Stack implementation in C++, using linkedlist.
 ** Last In First Out (LIFO)
 */
-class Stack{
+class Stack {
 
 public:
-      Stack();
-      void push(void *data);
-      void pop();
-      void print();
+	Stack();
+	void  push(void *data);
+	void* pop();
+	void  print();
 
 protected:
-      typedef struct Element{
-        void           *data;
-        struct Element *next;
-      } Element;
+	typedef struct Element {
+		void           *data;
+		struct Element *next;
+	} Element;
 
-      Element *top;
+	Element *top;
 };
 
 /* Constructor  */
-Stack::Stack( void )
+Stack::Stack(void)
 {
-  cout << "Stack Object is being created \n" ; 
-  top = NULL;
+	cout << "Stack Object is being created \n";
+	top = nullptr;
 }
 
 /**
@@ -36,39 +36,43 @@ Stack::Stack( void )
 **/
 void Stack::push(void *data)
 {
-  Element *elm = new Element;
+	Element *elm = new Element;
 
-  elm->data = data;
-  elm->next = top;
-  top = elm;
+	elm->data = data;
+	elm->next = top;
+	top = elm;
 };
 
 /**
 **  Pop item from top of stack
 **/
-void Stack::pop()
+void* Stack::pop()
 {
-  /* check stack is not empty,
-  ** take copy of the top, 
-  ** iterate stack,
-  ** print data and delete element
-  */
+	/* check stack is not empty,
+	** take copy of the top,
+	** iterate stack,
+	** print data and delete element
+	*/
 
-  Element *elm = new Element;
+	Element *elm;
 
-  //Check stack is not empty
-  if(top != NULL)
-  {
-    elm = top;
-    top = top->next;
-    int* intptr = static_cast<int*>(elm->data);
-    cout << "Pop item: " << *intptr << endl;
-  }else
-  {
-    cout << "Stack is Empty!"<< endl;
-  }
+	//Check stack is not empty
+	if (top != nullptr)
+	{
+		elm = top;
+		top = top->next;
+		#if 1
+		int* intptr = static_cast<int*>(elm->data);
+		cout << "Pop item: " << *intptr << endl;
+		#endif
+		return elm->data;
+	}
+	else
+	{
+		cout << "Stack is Empty!" << endl;
+	}
 
-  delete elm;
+	return nullptr;
 };
 
 /**
@@ -76,166 +80,262 @@ void Stack::pop()
 **/
 void Stack::print()
 {
-  Element *elm;
-  
-  if( top != NULL)
-  {
-    cout << "Stack: ";
-    elm = top;
-    //|_|->|_|->|_|->NULL
-    while(elm != NULL)
-    {
-      //Explicitly cast to another point type before dereferencing 
-      int *intPtr = static_cast<int*>(elm->data);
+	Element *elm;
 
-      cout << *intPtr << " -> ";
-      elm = elm->next;
-    }
-    cout << "NULL \n";
-  }else
-  {
-    cout << "Stack is empty !\n" ;
-  }
+	if (top != nullptr)
+	{
+		cout << "Stack: ";
+		elm = top; 
+		//|_|->|_|->|_|->nullptr
+		while (elm != nullptr)
+		{
+			//Explicitly cast to another point type before dereferencing 
+			int *intPtr = static_cast<int*>(elm->data);
+
+			cout << *intPtr << " -> ";
+			elm = elm->next;
+		}
+		cout << "nullptr \n";
+	}
+	else
+	{
+		cout << "Stack is empty !\n";
+	}
 };
 
 /*
 ** Queue implementation in C++, using linkedlist.
 ** First In First Out (FIFO)
 */
-class Queue{
+class Queue {
 
 public:
-   Queue();
-   void  enqueue(void *data);
-   void  dequeue();
-   void  print();
+	Queue();
+	void   enqueue(void *data);
+	void*  dequeue();
+	void   print();
 private:
 
 protected:
-  typedef struct Element{
-  void           *data;
-  struct Element *next;
-  }Element;
+	typedef struct Element {
+		void           *data;
+		struct Element *next;
+	}Element;
 
-  Element *front;
-  Element *rear;
+	Element *front;
+	Element *rear;
 };
 
 //Constructor
-Queue::Queue( void )
+Queue::Queue(void)
 {
-  cout << "Queue Object is being created \n" ; 
-  front = NULL;
-  rear  = NULL;
+	cout << "Queue Object is being created \n";
+	front = nullptr;
+	rear  = nullptr;
 }
 
 void Queue::enqueue(void *data)
 {
-  Element *temp = new Element();
+	Element *temp = new Element();
 
-  temp->data = data;
-  temp->next = NULL;
+	temp->data = data;
+	temp->next = nullptr;
 
-  if(front == NULL)
-  {
-    front = temp;
-    rear = front;   
-  }else
-  { 
-    rear->next = temp;
-    rear = rear->next;
-  }
+	if (front == nullptr)
+	{
+		front = temp;
+		rear = front;
+	}
+	else
+	{
+		rear->next = temp;
+		rear = rear->next;
+	}
 }
 
 /* Dequeus and returns the first
 ** item on the queue.
 **/
-void Queue::dequeue()
+void* Queue::dequeue()
 {
-  Element *elm = new Element();
+	Element *elm;
 
-  if(front != NULL)
-  {
-      elm = front;
+	if (front != nullptr)
+	{
+		elm = front;
 
-      if(front == rear)//When last item is dequed
-      {
-        front = NULL;
-        rear  = NULL;
-      }else
-      {
-        front = front->next;
-      }
-        int* intptr = static_cast<int*>(elm->data);
-        cout << "Dequeued item: " << *intptr <<endl;
-  }
+		if (front == rear)//When last item is dequed
+		{
+			front = nullptr;
+			rear  = nullptr;
+		}
+		else
+		{
+			front = front->next;
+		}
+		#if 1
+		int* intptr = static_cast<int*>(elm->data);
+		cout << "Dequeued item: " << *intptr << endl;
+		#endif
 
-  delete elm;
+		return elm->data;
+	}
+
+	return nullptr;
 }
-
 
 /* Prints all items on the Queue as int
 **/
 void Queue::print()
 {
-  Element *elm;
-  elm = front;
+	Element *elm;
+	elm = front;
 
-  cout << "Queue: ";
-  while(elm != NULL)
-  {
-    int* intptr = static_cast<int*>(elm->data);
-    cout  << *intptr << " <- ";
+	cout << "Queue: ";
+	while (elm != nullptr)
+	{
+		int* intptr = static_cast<int*>(elm->data);
+		cout << *intptr << " <- ";
 
-    elm = elm->next;
-  }
-  cout << "NULL\n";
+		elm = elm->next;
+	}
+	cout << "nullptr\n";
 }
 
-/* 
+
+/* Various functions to test Stack
+**/
+void testStack1(void)
+{
+	Stack stack;
+
+	int n1 = 2;
+	int n2 = 4;
+	int n3 = 6;
+	int n4 = 8;
+	int n5 = 10;
+
+	stack.push(&n1);
+	stack.print();
+	stack.pop();
+	stack.print();
+	stack.push(&n2);
+	stack.push(&n3);
+	stack.push(&n4);
+	stack.push(&n5);
+
+	stack.print();
+	stack.pop();
+	stack.print();
+}
+void testStack2(void)
+{
+	Stack stack;
+
+	int n1 = 2;
+	int n2 = 4;
+	int n3 = 6;
+	int n4 = 8;
+	int n5 = 10;
+
+	stack.push(&n1);
+	stack.push(&n2);
+	stack.push(&n3);
+	stack.push(&n4);
+	stack.push(&n5);
+
+	stack.print();
+}
+void testStack3(int input)
+{
+	Stack stack;
+	int n1 = 5;
+
+	for (int i = 0; i < input; i++)
+	{
+		stack.push(&n1);
+	}
+
+	stack.print();
+
+	for (int i = 0; i < input; i++)
+	{
+		stack.pop();
+	}
+
+	stack.print();
+
+	for (int i = 0; i < input; i++)
+	{
+		stack.push(&n1);
+	}
+
+	stack.print();
+}
+/* Various functions to test Queue
+**/
+void testQueue1(void)
+{
+	Queue queue;
+	int n1 = 2;
+	int n2 = 4;
+	int n3 = 6;
+	int n4 = 8;
+	int n5 = 10;
+
+	queue.enqueue(&n1);
+	queue.print();
+	queue.dequeue();
+	queue.print();
+
+	queue.enqueue(&n2);
+	queue.enqueue(&n3);
+	queue.enqueue(&n4);
+	queue.enqueue(&n5);
+
+	queue.print();
+	queue.dequeue();
+	queue.print();
+}
+void testQueue2(int input)
+{
+	Queue queue;
+
+	int n1 = 5;
+
+	for (int i = 0; i < input; i++)
+	{
+		queue.enqueue(&n1);
+	}
+
+	queue.print();
+
+	for (int i = 0; i < input; i++)
+	{
+		queue.dequeue();
+	}
+
+	queue.print();
+
+	for (int i = 0; i < input; i++)
+	{
+		queue.enqueue(&n1);
+	}
+}
+
+/*
 **
 */
 int main()
 {
-  Stack Stack1;
+	testStack1();
+	testStack2();
+	testStack3(10000);
+	
+	cout << endl;
 
-  int n1 = 2;
-  int n2 = 4;
-  int n3 = 6;
-  int n4 = 8;
-  int n5 = 10;
+	testQueue1();
+	testQueue2(10000);
 
-  Stack1.push(&n1);
-  Stack1.print();
-  Stack1.pop();
-  Stack1.print();
-  Stack1.push(&n2);
-  Stack1.push(&n3);
-  Stack1.push(&n4);
-  Stack1.push(&n5);
-
-  Stack1.print();
-  Stack1.pop();
-  Stack1.print();
-
-  cout << endl;
-
-  Queue Queue1;
-
-  Queue1.enqueue(&n1);
-  Queue1.print();
-  Queue1.dequeue();
-  Queue1.print();
-
-  Queue1.enqueue(&n2);
-  Queue1.enqueue(&n3);
-  Queue1.enqueue(&n4);
-  Queue1.enqueue(&n5);
-
-  Queue1.print();
-  Queue1.dequeue();
-  Queue1.print();
-
-  return 0;
+	return 0;
 }
-
